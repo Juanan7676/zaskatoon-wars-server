1f32761aea97295fcabe15714cd41a9e5c10e715
Juanan7676 <juanan_cientifico@yahoo.es>
2015-01-22 20:46:14 +0100

    More work on Build task processing.

--- Proccessors/BUILD.cpp
+++ Proccessors/BUILD.cpp
@@ -12,6 +12,7 @@
 #include <cppconn\prepared_statement.h>
 #include <sstream>
 #include <string>
+#include "..\Util\metadataseparator.h"
 
 void tasks::ProccessBuild(int cityID,std::string field)
 {
@@ -27,5 +28,21 @@ void tasks::ProccessBuild(int cityID,std::string field)
 	memcpy(var1,field.c_str(),field.size());
 	comando << "SELECT * FROM city" << cityID << " WHERE FieldX=" << var1[0] << " AND FieldY=" << var1[1];
 	rst = stmt->executeQuery(comando.str()); rst->first();
-	
+	std::string Metadata = rst->getString("Metadata");
+	char *cadena = new char[255];
+	memcpy(cadena,Metadata.c_str(),Metadata.size());
+	Tag var3 = util::SeparateTags(cadena,0);
+	Tag var2 = util::SeparateTags(cadena,1);
+	if (var2.TagValue == "1")
+	{
+		//Code to build the city
+	}
+	else
+	{
+		std::stringstream compose;
+		int time = atoi(var2.TagValue.c_str());
+		compose << "Building=" << var3.TagValue << ";RemainingTime=" << time - 1;
+		comando.str("");
+		comando << "UPDATE city" << cityID << " SET Metadata='" << compose.str() << "' WHERE FieldX=" << var1[0] << " AND FieldY=" << var1[1];
+	}
 }
